cf26939ed496ea0423a22b8f2612194ece22c2a6
Saïd <sayid.mimouni@gmail.com>
2017-10-20 23:39:32 +0200

    Setup XO

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 'use strict'
 
-const fs = require('fs'),
-      yaml = require('yamljs')
+const fs = require('fs')
+const yaml = require('yamljs')
 
 /**
  * Sets an environment variable (stores 2 version; one lowercased & the other uppercased)
@@ -19,19 +19,23 @@ const set = (key, value) => {
  * Raises an error if the file env.yml does not exist
  */
 fs.readFile('./env.yml', 'utf8', (err, data) => {
-  if (err) throw new Error('Barricade: the file env.yml does not exist. Please create one at the root of your application.')
+  if (err) {
+    throw new Error('Barricade: the file env.yml does not exist. Please create one at the root of your application.')
+  }
 
   // Parse the YAML file
   const env = yaml.parse(data)
 
   // Loop through the YAML file and assign each variable to the process.env object
-  for (let prop in env) {
-    if (env.hasOwnProperty(prop)) {
-      if (typeof env[prop] != 'object') {
+  for (const prop in env) {
+    if (Object.prototype.hasOwnProperty.call(env, prop)) {
+      if (typeof env[prop] !== 'object') {
         set(prop, env[prop])
-      } else if (typeof env[prop] == 'object' && prop == process.env.NODE_ENV) {
-        for (let prop2 in env[prop]) {
-          if (env[prop].hasOwnProperty(prop2)) set(prop2, env[prop][prop2])
+      } else if (typeof env[prop] === 'object' && prop === process.env.NODE_ENV) {
+        for (const prop2 in env[prop]) {
+          if (Object.prototype.hasOwnProperty.call(env[prop], prop2)) {
+            set(prop2, env[prop][prop2])
+          }
         }
       }
     }
--- test/barricade.test.js
+++ test/barricade.test.js
@@ -1,29 +1,28 @@
-'use strict2'
+'use strict'
 
-const expect  = require('chai').expect,
-      fs      = require('fs'),
-      yaml    = require('yamljs')
+const fs = require('fs')
+
+const expect = require('chai').expect
+const yaml = require('yamljs')
 
 describe('Barricade', () => {
   beforeEach(() => {
     // Empty the require.cache before each test runs
     Object.keys(require.cache).forEach(key => delete require.cache[key])
-   
-   // Unset the env variable also
+
+    // Unset the env variable also
     delete process.env.FOO
   })
 
   it('raises an error if the file env.yml does not exist', () => {
     fs.readFile = (filename, encoding, cb) => cb('file does not exist')
 
-    expect(function() {
+    expect(() => {
       require('../index')
     }).to.throw()
   })
 
   it('assigns the file content to the process.ENV object', () => {
-    expect(process.env.FOO).to.be.undefined
-
     // Mock the .env.yml file
     fs.readFile = (filename, encoding, cb) => cb(null, yaml.stringify({foo: 'bar'}, 2))
 
@@ -33,8 +32,6 @@ describe('Barricade', () => {
   })
 
   it('parses the file content based on the NODE_ENV', () => {
-    expect(process.env.FOO).to.be.undefined
-
     // Second one should overwrite the first one
     fs.readFile = (filename, encoding, cb) => cb(null, yaml.stringify({foo: 'bar'}, 2))
     fs.readFile = (filename, encoding, cb) => cb(null, yaml.stringify({test: {foo: 'test_bar'}}, 2))
@@ -44,8 +41,6 @@ describe('Barricade', () => {
   })
 
   it('transforms all the keys to the uppercase format', () => {
-    expect(process.env.FOO).to.be.undefined
-
     fs.readFile = (filename, encoding, cb) => cb(null, yaml.stringify({Foo: 'bar'}, 2))
     require('../index')
 
@@ -54,8 +49,6 @@ describe('Barricade', () => {
   })
 
   it('works with the lowercase version of the variable name', () => {
-    expect(process.env.FOO).to.be.undefined
-
     fs.readFile = (filename, encoding, cb) => cb(null, yaml.stringify({foo: 'bar'}, 2))
     require('../index')
 
